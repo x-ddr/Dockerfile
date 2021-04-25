343787d37528954c60e2d8ec45d80d28aaf33be6
anonymous <anonymous@gmail.com>
2021-04-25 19:07:13 +0800

    update

---
+++ nodejs/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+if [ ! -d /root/log ]; then
+  mkdir -p /root/log
+fi
+
+crond
+
+if [ -f /root/crontab.list ]; then
+  crontab /root/crontab.list
+fi
+
+if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
+  set -- node "$@"
+fi
+
+exec "$@"
