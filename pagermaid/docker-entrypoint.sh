9395d6caa07072d566da326c4619651af2151b61
anonymous <anonymous@hotmail.com>
2021-04-27 13:50:53 +0800

    update

--- pagermaid/docker-entrypoint.sh
+++ pagermaid/docker-entrypoint.sh
@@ -12,9 +12,7 @@ if [ -f pagermaid.session ]; then
     python -m pagermaid
 else
     echo "\"pagermaid.session\" 文件还没有生成，可能是首次部署容器，请先在映射目录下修改好 \"config.yml\" 后，进入容器手动运行一次 \"python -m pagermaid\"，并按照提示输入必要的信息，然后重启本容器。"
-    while true; do
-        sleep 1
-    done
+    while :; do sleep 1; done
 fi
 
 exec "$@"
